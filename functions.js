1ee1e0ad8eb68e0ffd209cedac6b32afa1940972
Ozan <okoc0876@uni.sydney.edu.au>
2025-07-08 20:18:30 +1000

    Wrote a toggle that changes the theme :).

---
+++ functions.js
@@ -0,0 +1,12 @@
+function ToggleTheme() {
+
+  var themeStylesheet = document.getElementById("theme-style");
+  var themeCheckbox = document.getElementById("theme-toggle");
+
+  if (themeCheckbox.checked === true) {
+    themeStylesheet.setAttribute("href", "dark-theme.css");
+  }
+  else {
+    themeStylesheet.setAttribute("href", "light-theme.css");
+  }
+}
